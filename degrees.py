8fb52ba8f901f2672e68888c911b9a1196116c17
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-11-21 15:27:52 -0800

    Update degrees.py

--- degrees.py
+++ degrees.py
@@ -8,7 +8,7 @@ for i in range(int(sys.argv[1])):
     b.append(i)
 
 if sys.argv[2] == "deg":
-    W = lambda x:  print(str(t.forward(x % 10 + sys.argv[3])) + str(t.left(math.degrees(x) % sys.argv[4]))) #+ "number " + str(x) + "\r\n" + str(math.degrees(x)) + " Degrees" + "\r\n"  + str(math.radians(x)) + " Radians" + "\r\n" + str(math.sinh(x)) + " Hyperbolic Sines" + "\r\n" + str(math.asinh(x)) + " Inverse Hyperbolic Sines" + "\r\n")
+    W = lambda x:  print(str(t.forward(x % 10 + int(sys.argv[3]))) + str(t.left(math.degrees(x) % int(sys.argv[4]) + int(sys.argv[3])))) #+ "number " + str(x) + "\r\n" + str(math.degrees(x)) + " Degrees" + "\r\n"  + str(math.radians(x)) + " Radians" + "\r\n" + str(math.sinh(x)) + " Hyperbolic Sines" + "\r\n" + str(math.asinh(x)) + " Inverse Hyperbolic Sines" + "\r\n")
 if sys.argv[2] == "rad":
-    W = lambda x:  print(str(t.forward(x % 10 + sys.argv[3])) + str(t.left(math.radians(x) ** 4 % sys.argv[4])))
+    W = lambda x:  print(str(t.forward(x % 10 + int(sys.argv[3]))) + str(t.right(math.radians(x) % int(sys.argv[4]) + int(sys.argv[3]))))
 print(list(map(W, b)))
